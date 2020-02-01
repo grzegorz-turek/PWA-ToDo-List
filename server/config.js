e66589c4a4bcaec1ae7a9a8b2bf57cecb06d9563
Grzegorz Turek <grzegorz.s.turek@gmail.com>
2020-02-01 18:44:48 +0100

    Fixed server config for heroku config vars

--- server/config.js
+++ server/config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    PORT: 8000,
     //DB: 'mongodb://localhost:27017/list-app'
-    DB: 'mongodb+srv://rhino-gt:sfR43rT53eg3Ed35ef1eTE43@cluster0-t5pho.mongodb.net/test?retryWrites=true&w=majority'
+    //PORT: 8000,
+    //DB: 'mongodb+srv://rhino-gt:sfR43rT53eg3Ed35ef1eTE43@cluster0-t5pho.mongodb.net/test?retryWrites=true&w=majority'
+    PORT: process.env.PORT,
+    DB: process.env.DB
 };
